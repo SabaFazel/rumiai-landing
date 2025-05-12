58bc653dd43707478778fdf6a3fbd843967e1d02
SabaFazel <saba.fazel@gmail.com>
2025-05-11 22:54:04 -0700

    Install supabase-js and add supabaseClient

--- pages/lib/supabaseClient.js
+++ pages/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://qeevvpzyhjlejchujkia.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlZXZ2cHp5aGpsZWpjaHVqa2lhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ0NDkxODYsImV4cCI6MjA2MDAyNTE4Nn0.R6qhqEn02BKqDPXflnJ3Ixh4YznVYrCgNsXw0q0x6jo';
+const supabaseUrl = process.env.https://qeevvpzyhjlejchujkia.supabase.co;
+const supabaseKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlZXZ2cHp5aGpsZWpjaHVqa2lhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ0NDkxODYsImV4cCI6MjA2MDAyNTE4Nn0.R6qhqEn02BKqDPXflnJ3Ixh4YznVYrCgNsXw0q0x6jo;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
